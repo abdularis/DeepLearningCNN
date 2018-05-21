df9eede3bc53ac2fc9c4e8aac026e6e7ab05c949
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-05-22 03:58:59 +0700

    fixed cnn trainer

--- trainer.py
+++ trainer.py
@@ -10,10 +10,10 @@ INPUT_SHAPE = [None, 128, 128, 3]
 NUM_CLASSES = 6
 LEARNING_RATE = 1e-4
 
-NUM_EPOCHS = 200
-BATCH_SIZE = 128
+NUM_EPOCHS = 50
+BATCH_SIZE = 64
 DATASET_FILENAME = 'dataset/dataset.h5'
-MODEL_FILENAME = './model/aar_net.ckpt'
+MODEL_FILENAME = './model/aar_net'
 RUN_NAME = '1'
 
 model = cnn.build_model_arch(INPUT_SHAPE, NUM_CLASSES, LEARNING_RATE)
@@ -30,15 +30,19 @@ with tf.Session() as sess:
         for epoch in range(NUM_EPOCHS):
             for step in range(data_set.batch_count):
                 batch_images, batch_labels = data_set.next_batch()
-                train_accuracy, loss, summary = model.train_step(sess, batch_images, batch_labels, run_summary=True)
-                print('Epoch %d, step %d, global step %d, training accuracy: %f, training loss %f'
-                      % (epoch, step, global_step, train_accuracy, loss))
-                file_writer.add_summary(summary, global_step)
+                if step % 50 == 0:
+                    train_accuracy, loss, summary = model.train_step(sess, batch_images, batch_labels, run_summary=True)
+                    file_writer.add_summary(summary, global_step)
+                else:
+                    train_accuracy, loss = model.train_step(sess, batch_images, batch_labels, run_summary=False)
+                    print('Epoch %d, step %d, global step %d, training accuracy: %f, training loss %f'
+                          % (epoch, step, global_step, train_accuracy, loss))
                 global_step += 1
 
             val_images, val_labels = data_set.val_data()
-            validation_accuracy = model.evaluate(sess, val_images, val_labels)
-            print('\tValidation accuracy: %f' % validation_accuracy)
+            val_accuracy, loss, summary = model.evaluate(sess, val_images, val_labels)
+            print('\tValidation accuracy: %f, loss %f' % (val_accuracy, loss))
+            file_writer.add_summary(summary, epoch)
             saver.save(sess, save_path=MODEL_FILENAME, global_step=epoch)
     except KeyboardInterrupt:
         print('Training cancelled intentionally.')
