fab8b185881a45448b76254c3fa83f0e523504a3
Abdul Aris <abdularisrahmanudin10@gmail.com>
2018-07-19 16:30:06 +0700

    implement gallery browser feature on web demo

--- web/api.py
+++ web/api.py
@@ -2,14 +2,10 @@
 # Created by abdularis on 16/07/18
 
 from flask import Flask, request, jsonify, g, url_for, render_template
-from skimage import transform
-import numpy as np
 import scipy.misc
+import os
 import sqlite3
-import model_client
-import data_config as cfg
 import image_search
-import distance_metrics
 
 app = Flask(__name__)
 
@@ -49,12 +45,25 @@ def search():
 
 @app.route('/')
 def index():
+    if request.args.get('search_by_gallery', ''):
+        gallery_path = request.args.get('search_by_gallery')
+        img_path = os.path.join('web/static/gallery/', gallery_path)
+        img = scipy.misc.imread(img_path)
+        probs_labels, images_result = image_search.search_image(img, get_db())
+        return render_template('index.html', title='Image Search - search by gallery',
+                               gallery_path=os.path.join('static/gallery/', gallery_path),
+                               pred_labels=probs_labels[0], img_path_list=images_result)
     return render_template('index.html', title='Image Search')
 
 
 @app.route('/browse')
 def browse():
-    return render_template('browse.html', title='Browse Gallery')
+    if request.args.get('class', ''):
+        cat = request.args.get('class')
+        img_list = os.listdir('web/static/gallery/%s' % cat)
+        img_list = [os.path.join(cat, i_name) for i_name in img_list]
+        return render_template('browse.html', title='Gallery - %s' % cat, img_path_list=img_list, category=cat)
+    return render_template('browse.html', title='Gallery')
 
 
 app.run(
